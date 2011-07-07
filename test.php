eebf6950c947304f254bb81134b5a7fe761eab59
Damien Hogan <damien.hogan@falmouth.ac.uk>
2011-07-07 14:02:26 +0100

    Added the input page

---
+++ test.php
@@ -0,0 +1,26 @@
+
+  <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  
+<script>
+	$(function() {
+		$( "#datepicker" ).datepicker();
+	});
+</script>
+
+
+<form method='post' action='schedule.php'>
+    <input type="text" name="enddate" id=""/>
+    <input type="submit">
+</form>
+
+
+
+
+
+
+<?php
+
+
+?>
